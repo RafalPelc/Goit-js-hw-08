4e32b98ce44a6bb12df76eb13813f9360021d761
RafalPelc <rafal.pelc2211@gmail.com>
2023-07-12 16:42:28 +0200

    another change

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,25 +1,47 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+console.log(SimpleLightbox);
+
+// Add imports above this line
+import { galleryItems } from './gallery-items';
 // Change code below this line
+
 const gallery = document.querySelector('.gallery');
-const images = galleryItems
-  .map(
-    image =>
-      `<div class = "gallery__item">
-      <a class = "gallery__link" href = "${image.original}">
-      <img class = "gallery__image"
-      data-source = "${image.original}"
-      src = "${image.preview}"
-      alt = "${image.description}"/>
-      </a>
-      </div>`
-  )
-  .join('');
-gallery.insertAdjacentHTML('beforeend', images);
-const lightbox = new SimpleLightbox('.gallery a', {
+
+createGalleryItems();
+
+function createGalleryItems() {
+  for (const galleryItem of galleryItems) {
+    const galleryLi = document.createElement('li');
+    galleryLi.classList.add('gallery__li');
+    gallery.append(galleryLi);
+
+    const galleryLink = document.createElement('a');
+    galleryLink.classList.add('gallery__item');
+    galleryLink.href = galleryItem.original;
+    galleryLi.append(galleryLink);
+
+    const galleryImage = document.createElement('img');
+    galleryImage.classList.add('gallery__image');
+    galleryImage.src = galleryItem.preview;
+    galleryImage.alt = galleryItem.description;
+    galleryImage.dataset.source = galleryItem.original;
+    galleryLink.append(galleryImage);
+  }
+}
+
+new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
+  scrollZoom: false,
 });
-console.log(gallery);
+
+const openImage = event => {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+};
+
+gallery.addEventListener('click', openImage);
+console.log(galleryItems);
