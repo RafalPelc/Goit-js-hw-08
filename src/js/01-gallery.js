b0699674799a012575ff82c0bb0b68c7f154b3b6
RafalPelc <rafal.pelc2211@gmail.com>
2023-07-12 16:31:13 +0200

    HW8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,40 +1,25 @@
-import 'simplelightbox/dist/simple-lightbox.min.css';
-import SimpleLightbox from 'simplelightbox';
-
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 const gallery = document.querySelector('.gallery');
-galleryItems.forEach(el => {
-  const li = document.createElement('li');
-  const a = document.createElement('a');
-  const img = document.createElement('img');
-
-  a.classList.add('gallery__link');
-  img.classList.add('gallery__image');
-
-  gallery.append(li);
-  li.append(a);
-  a.append(img);
-
-  a.href = el.original;
-  img.src = el.preview;
-  img.alt = el.description;
-});
-let lightbox = new SimpleLightbox('.gallery a', {
+const images = galleryItems
+  .map(
+    image =>
+      `<div class = "gallery__item">
+      <a class = "gallery__link" href = "${image.original}">
+      <img class = "gallery__image"
+      data-source = "${image.original}"
+      src = "${image.preview}"
+      alt = "${image.description}"/>
+      </a>
+      </div>`
+  )
+  .join('');
+gallery.insertAdjacentHTML('beforeend', images);
+const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
-  captionType: Text,
-  scrollZoom: false,
 });
-
-const selectImage = event => {
-  event.preventDefault();
-  if (event.target.nodeName !== 'IMG') {
-    return;
-  }
-  lightbox.open(event.target);
-};
-gallery.addEventListener('click', selectImage);
-
-console.log(galleryItems);
+console.log(gallery);
