ae1f5ba281c88e92196ce5ec11c87b06329cfe59
RafalPelc <rafal.pelc2211@gmail.com>
2023-07-12 15:21:06 +0200

    HW8 code change

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,11 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const updateTime = pausedTime =>
+  localStorage.setItem('videoplayer-current-time', pausedTime.seconds);
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+player.on('timeupdate', throttle(updateTime, 1000));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,45 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = document.querySelector('input[name="email"]');
+const messageInput = document.querySelector('textarea[name="message"]');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+function getFeedback() {
+  const feedback = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  return feedback;
+}
+
+const saveToLocalStorage = e => {
+  try {
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(getFeedback()));
+  } catch (error) {
+    console.error('Error saving to localStorage:', error);
+  }
+};
+
+window.addEventListener('load', () => {
+  try {
+    const previousValue = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+    if (previousValue) {
+      emailInput.value = previousValue.email;
+      messageInput.value = previousValue.message;
+    }
+  } catch (error) {
+    console.error('Error reading from localStorage:', error);
+  }
+});
+const sumbitForm = event => {
+  event.preventDefault();
+  console.log(getFeedback());
+  emailInput.value = '';
+  messageInput.value = '';
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(getFeedback()));
+  // console.log(getFeedback());
+};
+
+form.addEventListener('input', throttle(saveToLocalStorage, 500));
+form.addEventListener('submit', sumbitForm);
